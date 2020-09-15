19efa031457bc3e79e94f19ae436d516ef73a881
Sahaj Kedia <64389239+sahajkedia@users.noreply.github.com>
2020-09-15 23:09:16 +0530

    Add files via upload

---
+++ router/router.js
@@ -0,0 +1,29 @@
+
+
+app.get('/' , (req,res) => {
+    res.sendFile('index.html');
+})
+app.get('/code.html' , (req,res) => {
+res.sendFile('code.html');
+})
+app.get('/design.html' , (req,res) => {
+    res.sendFile('design.html');
+})
+app.get('/eln.html' , (req,res) => {
+    res.sendFile('eln.html');
+})
+app.get('/damed.html' , (req,res) => {
+    res.sendFile('gamed.html');
+})
+app.get('/index.html' , (req,res) => {
+    res.sendFile('index.html');
+})
+app.get('/python.html' , (req,res) => {
+    res.sendFile('python.html');
+})
+app.get('/quantum.html' , (req,res) => {
+    res.sendFile('quantum.html');
+})
+app.get('/webd.html' , (req,res) => {
+    res.sendFile('webd.html');
+})
